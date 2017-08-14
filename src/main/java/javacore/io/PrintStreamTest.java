baa0ed5c3c0b561d1260ef8737afcebc9df6cd52
Serezha <dergachov.serezha@gmail.com>
2017-08-14 16:45:02 +0400

    Java IO, add classes: PrintStreamTest PrintWriterTest

---
+++ src/main/java/javacore/io/PrintStreamTest.java
@@ -0,0 +1,23 @@
+package javacore.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class PrintStreamTest {
+    public static void main(String[] args) throws IOException {
+        //PrintStream to Console
+        PrintStream psConsole = new PrintStream(System.out);
+
+        psConsole.write("PrintStream to Console".getBytes());
+        psConsole.flush();
+        psConsole.close();
+
+        //PrintStream to File
+        PrintStream psFile = new PrintStream(new File("/home/serezha/javaioread4.txt"));
+
+        psFile.write("PrintStream write to file /home/serezha/javaioread4.txt".getBytes());
+        psFile.flush();
+        psFile.close();
+    }
+}
---
+++ src/main/java/javacore/io/PrintWriterTest.java
@@ -0,0 +1,26 @@
+package javacore.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PrintWriterTest {
+    public static void main(String[] args) throws IOException {
+
+        //PrintWriter to file
+        PrintWriter pwFile = new PrintWriter(new File("/home/serezha/javaioread4.txt"));
+
+        pwFile.write("PrintWriter write to file /home/serezha/javaioread4.txt");
+        pwFile.flush();
+        pwFile.close();
+
+        //PrintWriter to console
+        PrintWriter pwConsole = new PrintWriter(System.out);
+
+        pwConsole.write("PrintWriter write to Console");
+        pwConsole.flush();
+        pwConsole.close();
+
+
+    }
+}
